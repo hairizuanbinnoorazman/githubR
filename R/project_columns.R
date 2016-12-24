adeca5733f59c80cda5cf59875e645bdb82baafa
Hairizuan <hairizuanbinnoorazman@gmail.com>
2016-12-24 23:31:31 +0800

    Added the create project column function.

--- R/project_columns.R
+++ R/project_columns.R
@@ -30,4 +30,21 @@ get_project_column <- function(project_column_id){
   return(result_list)
 }
 
+#' Create a Column in a Project
+#' @importFrom httr config add_headers GET
+#' @export
+create_project_column <- function(project_id, column_name){
+  url <- paste0('https://api.github.com/projects/', project_id, '/columns')
+  # Get token
+  token <- get_token()
+  config <- httr::config(token=token)
+  headers <- httr::add_headers(Accept = 'application/vnd.github.inertia-preview+json')
+  # Wrapping body parameters in a requests list
+  body_params <- list(name = column_name)
+  # Try result
+  result <- httr::POST(url, config = config, headers, body = body_params, encode = "json")
+  result_content <- content(result, "text")
+  result_list <- fromJSON(result_content)
+  return(result_list)
+}
 
