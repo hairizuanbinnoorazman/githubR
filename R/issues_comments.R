c1631f55c90f81a9aad655c1bb0e25861611e778
Hairizuan <hairizuanbinnoorazman@gmail.com>
2017-02-01 00:55:26 +0800

    feat: Added function to create comment in an issue

--- R/issues_comments.R
+++ R/issues_comments.R
@@ -20,3 +20,29 @@ list_issues_comments <- function(owner, repo, number, since = NULL){
   result_list <- fromJSON(result_content)
   return(result_list)
 }
+
+#' Create Comment in an Issue
+#' @param owner The owner of the repository. In a repo name, it would be the part before the slash.
+#' @param repo The name of the repository
+#' @param number Issue Number (NOT issue ID)
+#' @param body The contents of the comment.
+#' @importFrom httr config add_headers GET
+#' @export
+list_issues_comments <- function(owner, repo, number, body = NULL){
+  url <- paste0('https://api.github.com/repos/', owner, '/', repo, '/issues/', number, '/comments')
+  # Body parameters
+  if(is.null(body)){
+    stop("Empty body in comment. Body is required field")
+  }
+  body_params <- list()
+  body_params['body'] <- body
+  # Get token
+  token <- get_token()
+  config <- httr::config(token=token)
+  headers <- httr::add_headers(Accept = 'application/vnd.github.v3+json')
+  # Try result
+  result <- httr::POST(url, config = config, headers, body = body_params, encode = "json")
+  result_content <- content(result, "text")
+  result_list <- fromJSON(result_content)
+  return(result_list)
+}
