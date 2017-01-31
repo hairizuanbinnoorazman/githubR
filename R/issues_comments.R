06ca8ee8ab4425ed5afce4abd9c5e3a25773986f
Hairizuan <hairizuanbinnoorazman@gmail.com>
2017-02-01 00:36:51 +0800

    feat: Added feature to view comments in an issue

---
+++ R/issues_comments.R
@@ -0,0 +1,22 @@
+#' List Comments in an Issue
+#' @param owner The owner of the repository. In a repo name, it would be the part before the slash.
+#' @param repo The name of the repository
+#' @param number Issue Number (NOT issue ID)
+#' @param since Only issues updated at or after this time are returned. Format is YYYY-MM-DD
+#' @importFrom httr config add_headers GET
+#' @export
+list_issues_comments <- function(owner, repo, number, since = NULL){
+  url <- paste0('https://api.github.com/repos/', owner, '/', repo, '/issues/', number, '/comments')
+  # Query parameters
+  query_params <- list()
+  query_params['since'] <- if(!is.null(since)){paste(as.Date(since), 'T00:00:00Z')} else{NULL}
+  # Get token
+  token <- get_token()
+  config <- httr::config(token=token)
+  headers <- httr::add_headers(Accept = 'application/vnd.github.v3+json')
+  # Try result
+  result <- httr::GET(url, config = config, headers, query = query_params, encode = "json")
+  result_content <- content(result, "text")
+  result_list <- fromJSON(result_content)
+  return(result_list)
+}
